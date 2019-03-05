322a92a8af6525c81f1df0c52312b68e8bbf0989
Vince Reuter <vince.reuter@gmail.com>
2019-03-05 17:58:50 -0500

    stubs

---
+++ tests/test_docstring_tags.py
@@ -0,0 +1,72 @@
+""" Tests for parsing and rendering docstrings that span multiple lines. """
+
+import pytest
+import oradocle
+
+__author__ = "Vince Reuter"
+__email__ = "vreuter@virginia.edu"
+
+
+@pytest.mark.skip("Not implemented")
+def test_no_tags():
+    """ When no tags are present, none are parsed. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_only_params():
+    """ When only return tag is present, it's used. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_only_returns():
+    """ When only return tag is present, it's used. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_multiple_returns():
+    """ Multiple return tags are prohibited. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_only_raises():
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_no_params():
+    """ When parameter tags are missing, none are parsed. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_no_returns():
+    """ When returns tag is missing, none is parsed. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_no_raises():
+    """ When exception tags are missing, none are parsed. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_golden_canonical():
+    """ All kinds of tags present, with/out both description sections. """
+    pass
+
+
+@pytest.mark.skip("Not imlpemented")
+def test_return_vs_returns():
+    """ Both styles of result docstring are allowed. """
+    pass
+
+
+@pytest.mark.skip("Not implemented")
+def test_raise_vs_raises():
+    """ Both styles of exception docstring are allowed. """
+    pass
